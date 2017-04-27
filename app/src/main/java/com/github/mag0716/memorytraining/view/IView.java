f7c6d3b7529d93e3c0c6d5c58bd38f1a3199ebb9
Masashi Kishimoto <mkishimoto0716@gmail.com>
2017-04-27 23:00:19 +0900

    add IView & IPresenter

---
+++ app/src/main/java/com/github/mag0716/memorytraining/view/IView.java
@@ -0,0 +1,13 @@
+package com.github.mag0716.memorytraining.view;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+/**
+ * Created by mag0716 on 2017/04/27.
+ */
+public interface IView {
+
+    @NonNull
+    public Context getContext();
+}
