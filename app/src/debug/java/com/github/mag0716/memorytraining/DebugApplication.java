f10e4cd9d8cd18525f6c7cf63e07d05f5be273a8
Masashi Kishimoto <mkishimoto0716@gmail.com>
2017-05-21 23:22:54 +0900

    insert test_data.json to DB.

--- app/src/debug/java/com/github/mag0716/memorytraining/DebugApplication.java
+++ app/src/debug/java/com/github/mag0716/memorytraining/DebugApplication.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 
 import com.facebook.stetho.Stetho;
+import com.github.gfx.android.orma.Inserter;
 import com.github.mag0716.memorytraining.model.Memory;
 import com.github.mag0716.memorytraining.model.OrmaDatabase;
 import com.squareup.moshi.JsonAdapter;
@@ -13,6 +14,10 @@ import com.squareup.moshi.Types;
 import java.io.IOException;
 import java.util.List;
 
+import io.reactivex.CompletableSource;
+import io.reactivex.ObservableSource;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
 import okio.BufferedSource;
 import okio.Okio;
 import timber.log.Timber;
@@ -37,9 +42,14 @@ public class DebugApplication extends Application {
                 .name(null) // In-Memory
                 .build();
 
-        // TODO: insert data
-        List<Memory> testData = loadTestData();
-        Timber.d("test data size = %d", testData.size());
+        // insert data
+        orma.prepareInsertIntoMemoryAsSingle()
+                .subscribeOn(Schedulers.io())
+                .flatMapObservable((Function<Inserter<Memory>, ObservableSource<?>>) memoryInserter
+                        -> memoryInserter.executeAllAsObservable(loadTestData()))
+                .subscribe(id -> {},
+                        Timber::e,
+                        () -> Timber.d("completed to insert test data."));
     }
 
     private List<Memory> loadTestData() {
@@ -48,6 +58,8 @@ public class DebugApplication extends Application {
         try {
             try (BufferedSource source = Okio.buffer(Okio.source(getAssets().open("test_data.json")))) {
                 return adapter.fromJson(source);
+
+                // TODO: add next_training_datetime
             }
         } catch (IOException e) {
             throw new IllegalStateException("test_data.json is invalid.");
