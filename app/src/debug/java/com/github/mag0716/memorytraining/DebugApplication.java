5715fe4881c3eed92b7aa689704628a3f97301bb
Masashi Kishimoto <mkishimoto0716@gmail.com>
2017-06-04 00:31:28 +0900

    debug 用 DB の初期化処理修正

--- app/src/debug/java/com/github/mag0716/memorytraining/DebugApplication.java
+++ app/src/debug/java/com/github/mag0716/memorytraining/DebugApplication.java
@@ -1,11 +1,14 @@
 package com.github.mag0716.memorytraining;
 
+import android.arch.persistence.room.Room;
 import android.content.Context;
 import android.support.annotation.NonNull;
 
 import com.annimon.stream.Stream;
 import com.facebook.stetho.Stetho;
 import com.github.mag0716.memorytraining.model.Memory;
+import com.github.mag0716.memorytraining.repository.database.ApplicationDatabase;
+import com.github.mag0716.memorytraining.repository.database.MemoryDao;
 import com.squareup.moshi.JsonAdapter;
 import com.squareup.moshi.Moshi;
 import com.squareup.moshi.Types;
@@ -13,8 +16,11 @@ import com.squareup.moshi.Types;
 import java.io.IOException;
 import java.util.List;
 
+import io.reactivex.Completable;
+import io.reactivex.schedulers.Schedulers;
 import okio.BufferedSource;
 import okio.Okio;
+import timber.log.Timber;
 
 /**
  * デバッグ用のアプリケーション情報
@@ -34,22 +40,26 @@ public class DebugApplication extends Application {
     }
 
     private void initializeDatabaseForDebug(@NonNull Context context) {
-//        if (orma != null) {
-//            throw new IllegalStateException("OrmaDatabase initialized already.");
-//        }
-//        orma = OrmaDatabase.builder(context)
-//                .name(null) // In-Memory
-//                .build();
-//
-//        // insert data
-//        orma.prepareInsertIntoMemoryAsSingle()
-//                .subscribeOn(Schedulers.io())
-//                .flatMapObservable((Function<Inserter<Memory>, ObservableSource<?>>) memoryInserter
-//                        -> memoryInserter.executeAllAsObservable(loadTestData()))
-//                .subscribe(id -> {
-//                        },
-//                        Timber::e,
-//                        () -> Timber.d("completed to insert test data."));
+        if (database != null) {
+            throw new IllegalStateException("Database initialized already.");
+        }
+
+        database = Room.inMemoryDatabaseBuilder(context, ApplicationDatabase.class).build();
+
+        // TODO: repository 経由でのアクセスに変更し、DB アクセスを隠蔽する
+        // insert debug data
+        Completable.create(
+                emitter -> {
+                    final MemoryDao memoryDao = database.memoryDao();
+                    List<Memory> memoryList = memoryDao.loadAll();
+                    if (memoryList.isEmpty()) {
+                        memoryDao.insertAll(loadTestData());
+                    }
+                    emitter.onComplete();
+                })
+                .subscribeOn(Schedulers.io())
+                .subscribe(() -> Timber.d("completed to insert debug data."),
+                        throwable -> Timber.w("failed to insert debug data.", throwable));
     }
 
     private List<Memory> loadTestData() {
