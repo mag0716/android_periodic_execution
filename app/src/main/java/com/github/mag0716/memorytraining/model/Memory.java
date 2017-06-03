3988e49386f315a10fa6bd99b77e5a3a478a45fd
Masashi Kishimoto <mkishimoto0716@gmail.com>
2017-06-03 23:31:31 +0900

    Room を Lombok と一緒に使う方法が分からないので Lombok を利用しない

--- app/src/main/java/com/github/mag0716/memorytraining/model/Memory.java
+++ app/src/main/java/com/github/mag0716/memorytraining/model/Memory.java
@@ -7,16 +7,12 @@ import android.arch.persistence.room.PrimaryKey;
 import android.os.Parcel;
 import android.os.Parcelable;
 
-import lombok.Setter;
-import lombok.experimental.Accessors;
-
 /**
  * 訓練対象データ
  * <p>
  * Created by mag0716 on 2017/04/30.
  */
 @Entity(indices = {@Index("level")})
-@Accessors(fluent = true)
 public class Memory implements Parcelable {
 
     /**
@@ -28,56 +24,75 @@ public class Memory implements Parcelable {
     /**
      * 質問
      */
-    @Setter
     private String question;
 
     /**
      * 回答
      */
-    @Setter
     private String answer;
 
     /**
      * 訓練レベル
      */
-    @Setter
     private int level;
 
     /**
      * 訓練回数
      */
-    @Setter
     private int count;
 
     /**
      * 次回訓練予定日時
      */
     @ColumnInfo(name = "next_training_datetime")
-    @Setter
     private long nextTrainingDatetime;
 
     public long getId() {
         return id;
     }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
     public String getQuestion() {
         return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
     }
 
     public String getAnswer() {
         return answer;
     }
 
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
     public int getLevel() {
         return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
     }
 
     public int getCount() {
         return count;
     }
 
+    public void setCount(int count) {
+        this.count = count;
+    }
+
     public long getNextTrainingDatetime() {
         return nextTrainingDatetime;
+    }
+
+    public void setNextTrainingDatetime(long nextTrainingDatetime) {
+        this.nextTrainingDatetime = nextTrainingDatetime;
     }
 
     // region Parcelable
