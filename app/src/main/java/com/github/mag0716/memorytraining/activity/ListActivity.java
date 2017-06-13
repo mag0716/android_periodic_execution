e9283b7c3a9ffbcd72e37178d2347b39d43b6290
Masashi Kishimoto <mkishimoto0716@gmail.com>
2017-06-13 23:18:35 +0900

    雑にレイアウトして、Memory データを反映する

--- app/src/main/java/com/github/mag0716/memorytraining/activity/ListActivity.java
+++ app/src/main/java/com/github/mag0716/memorytraining/activity/ListActivity.java
@@ -1,33 +1,65 @@
 package com.github.mag0716.memorytraining.activity;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.github.mag0716.memorytraining.Application;
 import com.github.mag0716.memorytraining.R;
+import com.github.mag0716.memorytraining.databinding.ActivityListBinding;
+import com.github.mag0716.memorytraining.model.Memory;
+import com.github.mag0716.memorytraining.repository.database.MemoryDao;
+import com.github.mag0716.memorytraining.view.adapter.MemoryListAdapter;
 
+import java.util.List;
+
+import io.reactivex.Single;
+import io.reactivex.SingleOnSubscribe;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+
+/**
+ * 訓練対象データ一覧画面
+ */
 public class ListActivity extends AppCompatActivity {
+
+    private ActivityListBinding binding;
+
+    private MemoryListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_list);
+        setSupportActionBar(binding.toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+
+        adapter = new MemoryListAdapter(this);
+        binding.content.trainingList.setLayoutManager(new LinearLayoutManager(this));
+        binding.content.trainingList.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        final MemoryDao memoryDao = ((Application) getApplication()).getDatabase().memoryDao();
+        Single.create((SingleOnSubscribe<List<Memory>>) emitter -> emitter.onSuccess(memoryDao.loadAll()))
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(memoryList -> adapter.addAll(memoryList));
     }
 
     @Override
---
+++ app/src/main/java/com/github/mag0716/memorytraining/view/adapter/MemoryListAdapter.java
@@ -0,0 +1,68 @@
+package com.github.mag0716.memorytraining.view.adapter;
+
+import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.github.mag0716.memorytraining.R;
+import com.github.mag0716.memorytraining.databinding.ViewListItemBinding;
+import com.github.mag0716.memorytraining.model.Memory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.Getter;
+
+/**
+ * Created by mag0716 on 2017/06/13.
+ */
+public class MemoryListAdapter extends RecyclerView.Adapter<MemoryListAdapter.MemoryViewHolder> {
+
+    private final LayoutInflater inflater;
+    private List<Memory> memoryList = new ArrayList<>();
+
+    public MemoryListAdapter(@NonNull Context context) {
+        inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public MemoryViewHolder onCreateViewHolder(ViewGroup viewGroup, int position) {
+        return new MemoryViewHolder(inflater.inflate(R.layout.view_list_item, viewGroup, false));
+    }
+
+    @Override
+    public void onBindViewHolder(MemoryViewHolder viewHolder, int position) {
+        // TODO: setVariable & executePendingBindings
+        final Memory memory = memoryList.get(position);
+        ((ViewListItemBinding) viewHolder.binding).questionText.setText(memory.getQuestion());
+    }
+
+    @Override
+    public int getItemCount() {
+        return memoryList.size();
+    }
+
+    public void addAll(List<Memory> memoryList) {
+        this.memoryList.clear();
+        if (memoryList != null) {
+            this.memoryList.addAll(memoryList);
+            notifyDataSetChanged();
+        }
+    }
+
+    public class MemoryViewHolder extends RecyclerView.ViewHolder {
+
+        @Getter
+        private final ViewDataBinding binding;
+
+        public MemoryViewHolder(View itemView) {
+            super(itemView);
+            binding = DataBindingUtil.bind(itemView);
+        }
+    }
+}
