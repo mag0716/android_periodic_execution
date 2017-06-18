a2d57f0a43042caee1152d98712370774f6e8f47
Masashi Kishimoto <mkishimoto0716@gmail.com>
2017-06-18 22:04:16 +0900

    開閉ボタンのアイコンを binding

--- app/src/main/java/com/github/mag0716/memorytraining/util/BindingUtil.java
+++ app/src/main/java/com/github/mag0716/memorytraining/util/BindingUtil.java
@@ -1,7 +1,10 @@
 package com.github.mag0716.memorytraining.util;
 
 import android.databinding.BindingAdapter;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.NonNull;
 import android.view.View;
+import android.widget.ImageView;
 
 /**
  * Created by mag0716 on 2017/06/17.
@@ -9,12 +12,17 @@ import android.view.View;
 public class BindingUtil {
 
     @BindingAdapter("visibleOrInvisible")
-    public static void setVisibleOrInvisible(View view, boolean visible) {
+    public static void setVisibleOrInvisible(@NonNull View view, boolean visible) {
         view.setVisibility(visible ? View.VISIBLE : View.INVISIBLE);
     }
 
     @BindingAdapter("visibleOrGone")
-    public static void setVisibleOrGone(View view, boolean visible) {
+    public static void setVisibleOrGone(@NonNull View view, boolean visible) {
         view.setVisibility(visible ? View.VISIBLE : View.GONE);
+    }
+
+    @BindingAdapter("srcCompat")
+    public static void setSrcCompat(@NonNull ImageView view, @DrawableRes int resourceId) {
+        view.setImageResource(resourceId);
     }
 }
--- app/src/main/java/com/github/mag0716/memorytraining/viewmodel/ListItemViewModel.java
+++ app/src/main/java/com/github/mag0716/memorytraining/viewmodel/ListItemViewModel.java
@@ -2,9 +2,11 @@ package com.github.mag0716.memorytraining.viewmodel;
 
 import android.databinding.BaseObservable;
 import android.databinding.Bindable;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
 
 import com.github.mag0716.memorytraining.BR;
+import com.github.mag0716.memorytraining.R;
 import com.github.mag0716.memorytraining.model.Memory;
 
 /**
@@ -37,9 +39,16 @@ public class ListItemViewModel extends BaseObservable {
     public void setShowingAnswer(boolean showingAnswer) {
         isShowingAnswer = showingAnswer;
         notifyPropertyChanged(BR.showingAnswer);
+        notifyPropertyChanged(BR.openAndCloseIcon);
     }
 
     public void toggleShowingAnswer() {
         setShowingAnswer(!isShowingAnswer);
+    }
+
+    @DrawableRes
+    @Bindable
+    public int getOpenAndCloseIcon() {
+        return isShowingAnswer ? R.drawable.ic_arrow_up_black_24dp : R.drawable.ic_arrow_down_black_24dp;
     }
 }
