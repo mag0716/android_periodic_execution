37239ac78e60e66d4916fe943e90e36f2bef0154
Masashi Kishimoto <mkishimoto0716@gmail.com>
2017-07-22 22:06:58 +0900

    ViewModel で選択中のカテゴリを保持する

--- app/src/main/java/com/github/mag0716/memorytraining/viewmodel/TrainingViewModel.java
+++ app/src/main/java/com/github/mag0716/memorytraining/viewmodel/TrainingViewModel.java
@@ -2,6 +2,7 @@ package com.github.mag0716.memorytraining.viewmodel;
 
 import android.databinding.BaseObservable;
 import android.databinding.Bindable;
+import android.support.annotation.IntRange;
 
 import com.github.mag0716.memorytraining.BR;
 
@@ -11,9 +12,30 @@ import com.github.mag0716.memorytraining.BR;
 public class TrainingViewModel extends BaseObservable {
 
     /**
+     * 訓練が必要なデータのみ
+     */
+    public static final int CATEGORY_ONLY_NEED_TRAINING = -1;
+    /**
+     * 全てのデータ
+     */
+    public static final int CATEGORY_ALL = -2;
+
+    /**
+     * 現在選択中のカテゴリ
+     */
+    private int currentCategory = CATEGORY_ONLY_NEED_TRAINING;
+    /**
      * 訓練データを追加可能かどうか
      */
     private boolean addable = true;
+
+    public int getCurrentCategory() {
+        return currentCategory;
+    }
+
+    public void setCurrentCategory(@IntRange(from = CATEGORY_ALL, to = CATEGORY_ONLY_NEED_TRAINING) int currentCategory) {
+        this.currentCategory = currentCategory;
+    }
 
     @Bindable
     public boolean isAddable() {
