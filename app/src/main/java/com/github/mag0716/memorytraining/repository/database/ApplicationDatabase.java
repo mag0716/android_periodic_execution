bcd94ab68c95aabcb3930d06305508a9fd49d76b
Masashi Kishimoto <mkishimoto0716@gmail.com>
2017-08-22 20:01:20 +0900

    Android Architecture Components のアップデートにより NOT NULL の指定が可能になったので、マイグレーション時にも指定しておく

--- app/src/main/java/com/github/mag0716/memorytraining/repository/database/ApplicationDatabase.java
+++ app/src/main/java/com/github/mag0716/memorytraining/repository/database/ApplicationDatabase.java
@@ -21,7 +21,7 @@ public abstract class ApplicationDatabase extends RoomDatabase {
     public static final Migration MIGRATION_1_2 = new Migration(1, 2) {
         @Override
         public void migrate(SupportSQLiteDatabase database) {
-            database.execSQL("ALTER TABLE Memory ADD COLUMN total_count INTEGER DEFAULT 0");
+            database.execSQL("ALTER TABLE Memory ADD COLUMN total_count INTEGER NOT NULL DEFAULT 0");
         }
     };
 
