9e94127ef6fd44ba140e424502ebe3285a82cdd7
Masashi Kishimoto <mkishimoto0716@gmail.com>
2017-08-24 22:05:18 +0900

    削除ボタンを削除メニューに置き換え

--- app/src/main/java/com/github/mag0716/memorytraining/fragment/EditFragment.java
+++ app/src/main/java/com/github/mag0716/memorytraining/fragment/EditFragment.java
@@ -6,6 +6,9 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -102,6 +105,21 @@ public class EditFragment extends Fragment implements EditView {
 
     // endregion
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_edit, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        final int id = item.getItemId();
+        if (id == R.id.action_delete) {
+            presenter.delete(memory);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     // region EditView
 
     @Override
@@ -110,6 +128,7 @@ public class EditFragment extends Fragment implements EditView {
         this.memory = memory;
         viewModel = new EditViewModel(memory);
         binding.setViewModel(viewModel);
+        setHasOptionsMenu(!memory.isNewData());
     }
 
     @Override
