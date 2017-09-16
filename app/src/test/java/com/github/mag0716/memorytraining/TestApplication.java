64b9be4ab451a2c0490e20d2f4bdc8f7ad861e02
Masashi Kishimoto <mkishimoto0716@gmail.com>
2017-09-16 23:51:43 +0900

    Firebase のトラッキングでテストに失敗していたのでテスト時はトラッキングを無効化

---
+++ app/src/test/java/com/github/mag0716/memorytraining/TestApplication.java
@@ -0,0 +1,9 @@
+package com.github.mag0716.memorytraining;
+
+public class TestApplication extends Application {
+
+    @Override
+    protected void setUpTracker() {
+        // テスト実行時には不要なのでトラッキングは無効化する
+    }
+}
